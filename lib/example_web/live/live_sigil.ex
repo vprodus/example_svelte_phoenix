a174c4f436503e68eb14c9ac1dc859007ebf8748
Vadim Vprodus <vproduspro@gmail.com>
2024-01-12 00:40:11 +0300

    added ssl connection to database

--- lib/example_web/live/live_sigil.ex
+++ lib/example_web/live/live_sigil.ex
@@ -3,25 +3,7 @@ defmodule ExampleWeb.LiveSigil do
 
   def render(assigns) do
     ~V"""
-    <script>
-      export let number
-      let number2 = 5
 
-      $: combined = number + number2
-    </script>
-
-    <h1 class="text-lg mb-4">Svelte template</h1>
-    {number} + {number2} = {combined}
-
-    <button phx-click="increment">+server</button>
-    <button on:click={() => number2 += 1}>+client</button>
-
-    <style lang="stylus">
-      button
-        background-color black
-        color white
-        padding 0.5rem 1rem
-    </style>
     """
   end
 
