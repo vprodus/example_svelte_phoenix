27e9b9a0834ea143bbff908e0f30119282b10715
Vadim Vprodus <vproduspro@gmail.com>
2024-01-16 21:32:01 +0300

    incoming host

--- lib/example_web/plugs/update_host_from_header_plug.ex
+++ lib/example_web/plugs/update_host_from_header_plug.ex
@@ -9,21 +9,21 @@ defmodule ExampleWeb.UpdateHostFromHeaderPlug do
 
   @impl true
   def call(conn, _opts) do
-    {_, apx_incoming_host} =
+    {_, incoming_host} =
       Enum.find(conn.req_headers, {false, false}, fn {header, _value} ->
         header == "apx-incoming-host"
       end)
 
-    IO.inspect(apx_incoming_host)
+    IO.inspect("received the incoming_host: #{incoming_host}")
 
-    case apx_incoming_host do
+    case incoming_host do
       false ->
         conn
 
       _ ->
         # sets the host for the request as the custom domain,
         # because the router will scope routes by matching hosts.
-        %Plug.Conn{conn | host: apx_incoming_host}
+        %Plug.Conn{conn | host: incoming_host}
     end
   end
 end
